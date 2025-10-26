a2ebcb0867d8df93db26c2c9d716cb6e1274dfaf
noklon <giomiqa2@gmail.com>
2025-10-26 05:49:20 +0400

    Initial

---
+++ src/main/java/lms/kiu/notifier/lms/nemo/NemoLmsNotifierApplication.java
@@ -0,0 +1,12 @@
+package lms.kiu.notifier.lms.nemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NemoLmsNotifierApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NemoLmsNotifierApplication.class, args);
+  }
+}
